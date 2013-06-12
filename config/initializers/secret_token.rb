50766cbf5919989a05dfae7e2603d5b705b5c552
Gerald S. Brown <gerbreown1@yahoo.com.ph>
2013-06-12 18:48:00 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PatTrkr::Application.config.secret_key_base = 'ede88f2f93205a2b9bf864fe604e931cef9bcd00a51dfeb210cda7d78c88a2c0c9ff9a06a9849d26bdf38107feb7af6b36095d4b17db79ca00ab4f5035558b3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PatTrkr::Application.config.session_store :cookie_store, key: '_pat_trkr_session'
